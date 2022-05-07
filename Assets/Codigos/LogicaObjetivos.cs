dc012b1e84e38ddccd13dcd26b6289db540b3750
ricardoarodriguez <ricardoa.rodriguez@gmail.com>
2022-05-07 02:03:31 -0500

    Implementación Misión de obtener esferas y puntos

---
+++ Assets/Codigos/LogicaObjetivos.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LogicaObjetivos : MonoBehaviour
+{
+    public int numDeObjetivos;
+    public TextMeshProUGUI textoMision;
+    public GameObject botonDeMision;
+
+    void Start()
+    {
+        numDeObjetivos = GameObject.FindGameObjectsWithTag("objetivo").Length;
+        textoMision.text = "Obten las esferas Rojas Restantes : "+numDeObjetivos; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "objetivo")
+        {
+            Destroy(col.transform.parent.gameObject);
+            numDeObjetivos--;
+            textoMision.text = "Obten las esferas Rojas Restantes: "+numDeObjetivos; 
+            if(numDeObjetivos <= 0 )
+            {
+                textoMision.text = "Completaste la Mision";
+                botonDeMision.SetActive(true);
+            }
+        }
+    }
+}
